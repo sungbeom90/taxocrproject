90b592edf4cb51161f5d2ba592d4be23b45bf8b5
sungbeom90 <sungbeom90@gmail.com>
2021-05-27 22:01:02 +0900

    Test: Add - Test case

--- model/detection/test.py
+++ model/detection/test.py
@@ -1,18 +1,16 @@
 import tensorflow.keras.backend as K
 
+x = img1
+y = img1_y
+
 x = K.constant(x)
 x = np.reshape(x, ((1,) + x.shape))
 y = np.reshape(y, ((1,) + y.shape))
 y = np.float32(y)
 y = K.constant(y)
 
-demo.compile(optimizer='adam',
-              loss=loss_region,
-              metrics=['accuracy'])
+demo.compile(optimizer="adam", loss=loss_region, metrics=["accuracy"])
 
 print(x.shape)
 print(y.shape)
-demo.fit(x,
-          y,
-          batch_size=1, 
-          epochs=1, verbose=True)
+demo.fit(x, y, batch_size=1, epochs=1, verbose=True)
