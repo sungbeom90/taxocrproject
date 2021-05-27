df226693c4692f68d7e9116062e27545f1833fd2
Ubuntu <ubuntu@ip-172-31-10-124.ap-northeast-2.compute.internal>
2021-05-27 01:04:53 +0000

    Fix: Add - LossFunction Tensorflow import

--- model/detection/lossFuntion.py
+++ model/detection/lossFuntion.py
@@ -1,4 +1,5 @@
 # 손실함수 - 기존 numpy.array 에서 tensor type으로 바꿈
+import tensorflow.keras.backend as K
 def loss_region(y_true, y_pred):
     result = K.sum(K.square(y_true - y_pred), axis=-1)
     return result
