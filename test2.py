680ab7db468a83efdf0f87f399555cda66b3aead
Goni-DA <gunzang1@gmail.com>
2021-05-28 15:58:38 +0900

    Test: check - iou function check

--- test2.py
+++ test2.py
@@ -2,8 +2,7 @@ import pickle
 import cv2
 import numpy as np
 from tests.detection import decoding
-import test
-
+from tests.detection import iou
 
 with open("./data/pickle/heatmap_0", "rb") as file:  # james.p 파일을 바이너리 읽기 모드(rb)로 열기
     data = pickle.load(file)
@@ -13,4 +12,6 @@ print(a)
 
 list = a
 
-test.TP 
+precision, recall = iou.TP_check(list,list)
+print("precision : {}, recall : {}".format(precision, recall))
+
