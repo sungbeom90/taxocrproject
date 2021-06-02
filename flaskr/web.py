60ec5f97e79b7da1e2d8fcf2278fffc7c51671c8
Goni-DA <gunzang1@gmail.com>
2021-06-02 17:03:30 +0900

    Feat: Add - completed hompage

--- flaskr/web.py
+++ flaskr/web.py
@@ -7,31 +7,23 @@ app = Flask(__name__)
 
 # 업로드된 파일주소가 저장되는 리스트
 upload_file_list = []
-
+app.config['UPLOAD_DIR'] = "./static/image/"
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-@app.route("/image_request", methods=["GET"])
-def image_input():
-    img = om.image_upload()
-    return render_template("service.html")
 
-
-@app.route("/guide", methods=["GET"])
-def guide():
-    return render_template("guide.html")
 
 
 @app.route("/done")
 def file_print():
     # 이미지 경로 설정
-    image_path1 = upload_file_list.pop()
+    image_upload = upload_file_list.pop()
     # image_path2는 text파일로 변동 예정임
-    image_path2 = "./image/after_0.jpg"
-    return render_template("done.html", image_file=image_path1, image_file2=image_path2)
+    #image_path2 = "./image/after_0.jpg"
+    return render_template("service.html", image_file=image_upload)
 
 
 @app.route("/uploader", methods=["GET", "POST"])
@@ -39,18 +31,18 @@ def upload_file():
     print("파일업로드 요청 접수됨")
     if request.method == "POST":
         # 파일 저장
-        f = request.files["cma_file"]
-        if f.content_length == 0:
-            priint("전달된 파일이 없어서 원래 페이지로 리다이렉트")
-            return redirect(url_for("image_input"))
-        else:
-            print("file storage 내부 : ", f)
-            f.save("./static/image/" + secure_filename(f.filename))
-            upfile_address = "image/" + secure_filename(f.filename)
-            upload_file_list.append(upfile_address)
-            print(upfile_address)
-            # 업로드된 파일명
-            return redirect(url_for("file_print"))
+        f = request.files["file"]
+        print("file storage 내부 : ", f)
+        f.save("./static/image/" + secure_filename(f.filename))
+        upfile_address = "image/" + secure_filename(f.filename)
+        upload_file_list.append(upfile_address)
+        print(upfile_address)
+        # 업로드된 파일명
+        return redirect(url_for("file_print"))
+
+@app.route("/guide", methods=["GET"])
+def guide():
+    return render_template("guide.html")
 
 
 @app.route("/predicted_img", methods=["POST"])
@@ -64,5 +56,5 @@ def Predict_img():
 
 if __name__ == "__main__":
     app.debug = True
-    # app.run(port=80)
-    app.run(host="3.35.53.198", port=80)
+    #app.run(port=80)
+    app.run(host="192.168.187.1", port=80, debug=True)
