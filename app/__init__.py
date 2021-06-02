387a99c300beb46e2ba001d0ae14f680cf9d0a0c
sungbeom90 <sungbeom90@gmail.com>
2021-06-02 12:28:18 +0900

    Test : Add - db conn

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,16 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/db")
+def select():
+    db_class = mod_dbconn.Database()
+    sql = "SELECT b_id \
+                FROM taxocr.t_bill"
+    row = db_class.executeAll(sql)
+    print(row)
+    return render_template("db.html", resultData=row[0])
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
