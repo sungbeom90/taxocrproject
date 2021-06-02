e97a555972ad12ae93ac443233e017e0ffd207cf
sungbeom90 <sungbeom90@gmail.com>
2021-06-02 10:28:56 +0900

    Test : Modify - add name

--- tests/application.py
+++ tests/application.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 # EB looks for an 'application' callable by default.
-application = Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -13,5 +13,5 @@ def home():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run(host="0.0.0.0")
+    app.debug = True
+    app.run(host="0.0.0.0")
