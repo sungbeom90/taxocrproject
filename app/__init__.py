ab372ae1cd40489a901e2a99135c484b7fdc0b76
sungbeom90 <sungbeom90@gmail.com>
2021-06-03 15:23:51 +0900

    Test : Add - insert bill

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, redirect, request, url_for
 from app import mod_dbconn
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
 @app.route("/db")
 def select():
     db_class = mod_dbconn.Database()
@@ -12,6 +17,7 @@ def select():
     row = db_class.executeAll(sql)
     print(row)
     return render_template("db.html", resultData=row[0])
+
 
 @app.route("/insert_provider", methods=("GET", "POST"))
 def insert_provider():
@@ -26,39 +32,49 @@ def insert_provider():
                 VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
         db_class.execute(query=sql, args=args)
         db_class.commit()
+        return render_template("home.html")
+
+
+@app.route("/insert_bill", methods=("GET", "POST"))
+def insert_provider():
+    print("계산서 등록 요청 접수됨")
+    if request.method == "GET":
+        return render_template("insert.html")
+    if request.method == "POST":
+        db_class = mod_dbconn.Database()
+        args = tuple(request.form.values())
+        print(args)
+        sql = """INSERT into taxocr.t_bill (b_id, b_date, b_mr, b_etc, b_cost_total, b_cost_sup, b_cost_tax,
+                                            b_cost_cash, b_cost_check, b_cost_note, b_cost_credit, FK_p_id)
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+        db_class.execute(query=sql, args=args)
+        db_class.commit()
+        return render_template("home.html")
+
 
 @app.route("/")
 def printHello():
     return "Hello World - Flask"
+
+
 @app.route("/bargraph")
-def barGraph(): # 받아오려면 매개변수 필요하겠지
-    title='bargraph'
-    labels=[]
-    data=[]
-    data2=[]
+def barGraph():  # 받아오려면 매개변수 필요하겠지
+    title = "bargraph"
+    labels = []
+    data = []
+    data2 = []
     # 미수금 데이터
     misoo = 60000000
-
-    #db 테스트
-    temp=[]
-    db_class = mod_dbconn.Database()
-    sql = "SELECT p_corp_name\
-                FROM taxocr.t_provider"
-    row = db_class.executeAll(sql)
-    print(row)
-
-    for i in range(len(row)):
-        temp.append(row[i])
-    
-    for i in temp:
-        temp[i] = (temp[i], '200')
-    
-    print(temp)
-
     # Doughnut graph
     # fetchall()로 넘어올 것
-    tupledata = (('A회사','300'),('B회사','500'),('C회사','600'),
-                ('D회사','1000'),('E회사','200'),('F회사','10'))
+    tupledata = (
+        ("A회사", "300"),
+        ("B회사", "500"),
+        ("C회사", "600"),
+        ("D회사", "1000"),
+        ("E회사", "200"),
+        ("F회사", "10"),
+    )
     # labels 뽑아오기
     for i in range(len(tupledata)):
         labels.append(tupledata[i][0])
@@ -69,24 +85,54 @@ def barGraph(): # 받아오려면 매개변수 필요하겠지
         data.append(tupledata[i][1])
     print(data)
     # DB에서 받아올 월별 거래량 데이터
-    monthlydata = ('200','300','600','100','500','100',
-                    '150','750','412','861','40','577')
+    monthlydata = (
+        "200",
+        "300",
+        "600",
+        "100",
+        "500",
+        "100",
+        "150",
+        "750",
+        "412",
+        "861",
+        "40",
+        "577",
+    )
     for i in range(len(monthlydata)):
         data2.append(monthlydata[i])
     print(data2)
-    # Bubble graph
-    return render_template('bargraph.html', title=title, misoo=misoo, labels=labels, data=data, data2=data2)  
-@app.route("/linegraph") 
+    # Scatter graph
+    return render_template(
+        "bargraph.html", title=title, misoo=misoo, labels=labels, data=data, data2=data2
+    )
+
+
+@app.route("/linegraph")
 def lineGraph():
-    title='linegraph'
+    title = "linegraph"
     data = []
     # DB에서 받아올 월별 거래량 데이터
-    monthlydata = ('200','300','600','100','500','100',
-                    '150','750','412','861','40','577')
+    monthlydata = (
+        "200",
+        "300",
+        "600",
+        "100",
+        "500",
+        "100",
+        "150",
+        "750",
+        "412",
+        "861",
+        "40",
+        "577",
+    )
     for i in range(len(monthlydata)):
         data.append(monthlydata[i])
     print(data)
-    return render_template('linegraph.html', title=title, data=data)
+    return render_template("linegraph.html", title=title, data=data)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 from app import app
