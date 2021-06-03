88d4f2b49a330f645788b5f9e84a634c5553b29e
최지영 <chooooiala@gmail.com>
2021-06-03 12:01:42 +0900

    Fix : Merge - init

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template
 from app import mod_dbconn
 
 app = Flask(__name__)
@@ -8,7 +8,6 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-
 @app.route("/db")
 def select():
     db_class = mod_dbconn.Database()
@@ -17,20 +16,18 @@ def select():
     row = db_class.executeAll(sql)
     print(row)
     return render_template("db.html", resultData=row[0])
-
-
+    
 @app.route("/insert_provider", methods=("GET", "POST"))
 def insert_provider():
     print("공급자 등록 요청 접수됨")
     if request.method == "GET":
         return render_template("insert.html")
-
     if request.method == "POST":
         db_class = mod_dbconn.Database()
         args = request.form.to_dict()
         print(args)
-        sql = "INSERT into taxocr.t_provider (p_id, p_crop_num, p_corp_name, p_ceo_name, p_add, p_stat, p_type, p_email) *\
-                VALUES (:p_id, :p_crop_num, :p_corp_name, :p_ceo_name, :p_add, :p_stat, :p_type, :p_email)"
+        sql = """INSERT into taxocr.t_provider (p_id, p_crop_num, p_corp_name, p_ceo_name, p_add, p_stat, p_type, p_email)
+                VALUES (:p_id, :p_crop_num, :p_corp_name, :p_ceo_name, :p_add, :p_stat, :p_type, :p_email)"""
         # args = {
         #     "p_id": p_id,
         #     "p_crop_num": p_corp_num,
@@ -44,71 +41,65 @@ def insert_provider():
         db_class.execute(sql, args)
         return render_template("home.html")
 
-
 @app.route("/")
 def printHello():
     return "Hello World - Flask"
 
-
 @app.route("/bargraph")
-def barGraph():  # 받아오려면 매개변수 필요하겠지
+def barGraph(): # 받아오려면 매개변수 필요하겠지
+    
+    title='bargraph'
+    labels=[]
+    data=[]
+    data2=[]
 
-    title = "bargraph"
-    labels = []
-    data = []
+    # 미수금 데이터
+    misoo = 60000000
 
+    # Doughnut graph
     # fetchall()로 넘어올 것
-    tupledata = (
-        ("A회사", "100"),
-        ("B회사", "500"),
-        ("C회사", "600"),
-        ("D회사", "1000"),
-        ("E회사", "200"),
-        ("F회사", "10"),
-    )
-
+    tupledata = (('A회사','300'),('B회사','500'),('C회사','600'),
+                ('D회사','1000'),('E회사','200'),('F회사','10'))
+    
     # labels 뽑아오기
     for i in range(len(tupledata)):
         labels.append(tupledata[i][0])
 
     print(labels)
 
+    # Line graph
     # data 뽑아오기
     for i in range(len(tupledata)):
         data.append(tupledata[i][1])
 
     print(data)
 
-    return render_template("bargraph.html", title=title, labels=labels, data=data)
+    # DB에서 받아올 월별 거래량 데이터
+    monthlydata = ('200','300','600','100','500','100',
+                    '150','750','412','861','40','577')
 
+    for i in range(len(monthlydata)):
+        data2.append(monthlydata[i])
+    print(data2)
 
-@app.route("/linegraph")
+    # Scatter graph
+    
+    return render_template('bargraph.html', title=title, misoo=misoo, labels=labels, data=data, data2=data2)  
+
+@app.route("/linegraph") 
 def lineGraph():
-    title = "linegraph"
+    title='linegraph'
     data = []
 
     # DB에서 받아올 월별 거래량 데이터
-    monthlydata = (
-        "200",
-        "300",
-        "600",
-        "100",
-        "500",
-        "100",
-        "150",
-        "750",
-        "412",
-        "861",
-        "40",
-        "577",
-    )
+    monthlydata = ('200','300','600','100','500','100',
+                    '150','750','412','861','40','577')
 
     for i in range(len(monthlydata)):
         data.append(monthlydata[i])
     print(data)
 
-    return render_template("linegraph.html", title=title, data=data)
-
+    return render_template('linegraph.html', title=title, data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
