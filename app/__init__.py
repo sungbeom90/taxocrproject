0fc9499e1f20e2ec9467864ae1a8db96628de15b
sungbeom90 <sungbeom90@gmail.com>
2021-06-03 13:53:06 +0900

    Test : Moniter - args

--- app/__init__.py
+++ app/__init__.py
@@ -26,10 +26,12 @@ def insert_provider():
         return render_template("insert.html")
     if request.method == "POST":
         db_class = mod_dbconn.Database()
-        args = request.form.lists
+        args = request.form.tupledata()
+        args2 = request.form.lists()
+        args3 = request.form.to_dict()
         print(args)
-        args = list(args)
-        print(args)
+        print(args2)
+        print(args3)
         sql = """INSERT into taxocr.t_provider (p_id, p_corp_num, p_corp_name, p_ceo_name, p_add, p_stat, p_type, p_email)
                 VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
         # args = {
