2c786f6c1fa8983d77baf9a3fc5e202bf1e6a847
최지영 <chooooiala@gmail.com>
2021-06-03 18:04:41 +0900

    Test : Modify - init DB connection

--- app/__init__.py
+++ app/__init__.py
@@ -78,26 +78,35 @@ def barGraph():  # 받아오려면 매개변수 필요하겠지
     labels = []  # 도넛그래프 x축 : 회사명
     data = []
     data2 = []
-    # 미수금 데이터
+    # 세금 데이터
     misoo = 60000000
 
     # db 테스트
     # Doughnut graph
     temp = []
+    temp2 = []
     temptuple = []
     db_class = mod_dbconn.Database()
-    sql = "SELECT p_corp_name\
-                FROM taxocr.t_provider"
+    sql = "SELECT @rownum:=@rownum+1, p_corp_name, b_cost_total_sum\
+            FROM (SELECT t_provider.p_corp_name as p_corp_name,\
+            SUM(t_bill.b_cost_total) as b_cost_total_sum\
+                FROM t_provider, t_bill\
+                WHERE t_provider.p_id = t_bill.FK_p_id \
+                GROUP BY FK_p_id\
+                ORDER BY SUM(t_bill.b_cost_total) DESC) inlineview\
+            WHERE (@rownum:=0)=0 AND @rownum <=5;"
     row = db_class.executeAll(sql)
     print(row)  # [{'p_corp_name':'주식회사 아이피스'},{'p_corp_name':'(주)타라그래픽스 동여의도점'}, ...]
 
     for i in row:
         temp.append(i["p_corp_name"])
+        temp2.append(i["b_cost_total_sum"])
     print(temp)
+    print(temp2)
 
     # 회사명, 거래금액
     for i in range(len(temp)):
-        a = (temp[i], "200")
+        a = (temp[i], temp2[i])
         temptuple.append(a)
     print(temptuple)
 
@@ -112,25 +121,7 @@ def barGraph():  # 받아오려면 매개변수 필요하겠지
         data.append(temptuple[i][1])
 
     print(data)
-
-    # fetchall()로 넘어올 것
-    tupledata = (
-        ("A회사", "300"),
-        ("B회사", "500"),
-        ("C회사", "600"),
-        ("D회사", "1000"),
-        ("E회사", "200"),
-        ("F회사", "10"),
-    )
-    # labels 뽑아오기
-    for i in range(len(tupledata)):
-        labels.append(tupledata[i][0])
-    print(labels)
-    # Line graph
-    # data 뽑아오기
-    for i in range(len(tupledata)):
-        data.append(tupledata[i][1])
-    print(data)
+    
     # DB에서 받아올 월별 거래량 데이터
     monthlydata = (
         "200",
