02530341d685ad64e3abc56ab2d6eeb2af9847ed
sungbeom90 <sungbeom90@gmail.com>
2021-06-07 17:59:06 +0900

    Fix : Modfiy - method

--- app/ocr_manage.py
+++ app/ocr_manage.py
@@ -90,42 +90,44 @@ def provider_exists(p_id):
     sql = """SELECT EXISTS (select * from t_provider where p_id=%(p_id)s) as success"""
     try:
         desc_dict = db_class.executeOne(sql, args=p_id)
-        print("{} 가 db에 있는지 결과 : {} ".format(p_id, desc_dict["success"]))
-        return desc_dict  # provider_exists 요청 성공
+        print("{} 가 db에 있는지 결과 : {} ".format(p_id["p_id"], desc_dict["success"]))
+        return desc_dict["success"]  # provider_exists 요청 성공
     except:
-        print("supply_exists 요청 실패")
+        print("provider_exists 요청 실패")
         return -1  # provider_exists 요청 실패
 
 
 # 공급자 입력(생성)하는 함수
 def provider_insert(t_provider):
     print("공급자 등록 요청 접수됨")
-    args = t_provider
-    print(args)
+    print(t_provider)
     sql = """INSERT into taxocr.t_provider (p_id, p_corp_num, p_corp_name, p_ceo_name, p_add, p_stat, p_type, p_email)
             VALUES (%(p_id)s,%(p_corp_num)s,%(p_corp_name)s,%(p_ceo_name)s,%(p_add)s,%(p_stat)s,%(p_type)s,%(p_email)s)"""
     try:
-        db_class.execute(query=sql, args=args)
+        db_class.execute(query=sql, args=t_provider)
         db_class.commit()
+        print("{} 공급자 db 등록 완료".format(t_provider["p_id"]))
         return 1  #  provider_insert 요청 성공
     except:
+        print("provider_insert 요청 실패")
         return -1  # provider_insert 요청 실패
 
 
 # 계산서 입력(생성)하는 함수
 def bill_insert(t_bill):
     print("계산서 등록 요청 접수됨")
-    args = t_bill
-    print(args)
+    print(t_bill)
     sql = """INSERT into taxocr.t_bill (b_id, b_date, b_mr, b_etc, b_cost_total, b_cost_sup, b_cost_tax,
                                         b_cost_cash, b_cost_check, b_cost_note, b_cost_credit, FK_p_id)
             VALUES (%(b_id)s,%(b_date)s,%(b_mr)s,%(b_etc)s,%(b_cost_total)s,%(b_cost_sup)s,
             %(b_cost_tax)s,%(b_cost_cash)s,%(b_cost_note)s,%(b_cost_credit)s,%(FK_p_id)s)"""
     try:
-        db_class.execute(query=sql, args=args)
+        db_class.execute(query=sql, args=t_bill)
         db_class.commit()
+        print("{} 계산서 db 등록 완료".format(t_provider["b_id"]))
         return 1  # bill_insert 요청 성공
     except:
+        print("bill_insert 요청 실패")
         return -1  # bill_insert 요청 실패
 
 
