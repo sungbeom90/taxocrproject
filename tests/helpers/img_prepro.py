06e8b23cd3bc50c21dc1081d676277a33223dd3d
sungbeom90 <sungbeom90@gmail.com>
2021-06-08 22:39:04 +0900

    Test : Modify - window name

--- tests/helpers/img_prepro.py
+++ tests/helpers/img_prepro.py
@@ -100,7 +100,7 @@ def detection_preprocess(image: Image):
 
     re2_img = cv2.threshold(re_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-    cv2.imshow("THRESH_BINARY_re", re_img)
+    cv2.imshow("THRESH_BINARY_re", re2_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
