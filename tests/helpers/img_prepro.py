45b4c7d643b6b9ad8612b069e522796f97f2b6b9
sungbeom90 <sungbeom90@gmail.com>
2021-06-08 22:46:49 +0900

    Test : Add - comment

--- tests/helpers/img_prepro.py
+++ tests/helpers/img_prepro.py
@@ -40,9 +40,7 @@ def detection_preprocess(image: Image):
 
     b_w = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-    cv2.imshow("THRESH_BINARY_INV", b_w)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imshow("THRESH_BINARY_INV", b_w)  # 텍스트와 표 그리드를 백, 나머지 배경을 흑
 
     contours, _ = cv2.findContours(b_w, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(b_w, contours, -1, (255), 3)
@@ -57,51 +55,41 @@ def detection_preprocess(image: Image):
     horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (100, 1))
     horizontal_img = cv2.erode(horizontal_img, horizontal_kernel, iterations=1)
 
-    cv2.imshow("y100 erode", horizontal_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imshow("y100 erode", horizontal_img)  # (100,1) 커널로 침식하여 가로줄 찾기
 
     horizontal_img = cv2.dilate(horizontal_img, horizontal_kernel, iterations=1)
 
-    cv2.imshow("y100 dilate", horizontal_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imshow("y100 dilate", horizontal_img)  # (100,1) 커널로 팽창
 
     vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 100))
     vertical_img = cv2.erode(vertical_img, vertical_kernel, iterations=1)
 
-    cv2.imshow("x100 erode", vertical_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imshow("x100 erode", vertical_img)  # (1,100) 커널로 침식하여 세로줄 찾기
 
     vertical_img = cv2.dilate(vertical_img, vertical_kernel, iterations=1)
 
-    cv2.imshow("x100 dilate", vertical_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imshow("x100 dilate", vertical_img)  # (1,100) 커널로 팽창
 
     mask_img = horizontal_img + vertical_img
 
-    cv2.imshow("img brodcasting", mask_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imshow("img brodcasting", mask_img)  # 가로 세로줄 합쳐 표 얻기 (표는 백색)
 
     b_w2 = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-    cv2.imshow("THRESH_BINARY", b_w2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imshow("THRESH_BINARY", b_w2)  # 텍스트와 표 그리드를 흑, 배경을 백
 
     re_img = np.bitwise_or(b_w2, mask_img)
 
-    cv2.imshow("THRESH_BINARY", re_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imshow("THRESH_BINARY", re_img)  # 비트 or 연산하여 표 제거 (흑 + 백 = 백색)
 
     re2_img = cv2.threshold(re_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-    cv2.imshow("THRESH_BINARY_re", re2_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    imshow("THRESH_BINARY_re", re2_img)  # 표가 제거된 이미지를 흑백 반전하여 텍스트를 백, 배경을 흑으로
 
     return Image.fromarray(re2_img)
+
+
+def imshow(img_name, img):
+    cv2.imshow(img_name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
