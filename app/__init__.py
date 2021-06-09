a9fd3045a1a34c5b787eac06769c5138b0c9c78c
sungbeom90 <sungbeom90@gmail.com>
2021-06-09 10:56:47 +0900

    Test

--- app/__init__.py
+++ app/__init__.py
@@ -118,8 +118,8 @@ def dashboard():
     # 세금 데이터----------------------------------------
     taxsql = """SELECT SUM(t_bill.b_cost_tax)
                 FROM t_bill
-                WHERE YEAR(t_bill.b_date) = %s """
-    taxrow = db_class.executeOne(taxsql, year_)
+                WHERE YEAR(t_bill.b_date) = '2010' """
+    taxrow = db_class.executeOne(taxsql)
 
     taxdata = int(taxrow["SUM(t_bill.b_cost_tax)"])
 
@@ -132,10 +132,10 @@ def dashboard():
     sql = """SELECT t_provider.p_corp_name as p_corp_name,
             SUM(t_bill.b_cost_total) as b_cost_total_sum
             FROM t_provider, t_bill
-            WHERE t_provider.p_id = t_bill.FK_p_id AND YEAR(t_bill.b_date) = '%s'
+            WHERE t_provider.p_id = t_bill.FK_p_id AND YEAR(t_bill.b_date) = '2010'
             GROUP BY FK_p_id
             ORDER BY SUM(t_bill.b_cost_total) DESC"""
-    row = db_class.executeAll(sql, year_)
+    row = db_class.executeAll(sql)
     print("fetchall row:{}".format(row))
     print("fetchall rowtype:{}".format(type(row)))
     print("fetchall rowlength:{}".format(len(row)))
@@ -166,11 +166,9 @@ def dashboard():
     # Line graph---------------------------------------------
     linesql = """SELECT MONTH(t_bill.b_date), SUM(t_bill.b_cost_total)
                 FROM t_bill
-                WHERE YEAR(t_bill.b_date) = '%s'
+                WHERE YEAR(t_bill.b_date) = '2010'
                 GROUP BY MONTH(t_bill.b_date)"""
-    linerow = db_class.executeAll(
-        linesql, year_
-    )  # [{'b_data':'1','b_cost_total':10000}, ...]
+    linerow = db_class.executeAll(linesql)  # [{'b_data':'1','b_cost_total':10000}, ...]
 
     print(linerow)
 
@@ -186,7 +184,7 @@ def dashboard():
                 SUM(t_bill.b_cost_note),
                 SUM(t_bill.b_cost_credit) 
         FROM t_bill
-        WHERE YEAR(t_bill.b_date) = '%s'"""
+        WHERE YEAR(t_bill.b_date) = '2010'"""
 
     barrow = db_class.executeAll(
         barsql
